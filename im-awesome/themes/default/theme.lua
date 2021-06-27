22f9348ec840025969e1de34fbed6dfe37a30595
igor-mircic <igor.mircic@live.com>
2021-06-27 12:48:18 +0200

    updated awesome config

--- im-awesome/themes/default/theme.lua
+++ im-awesome/themes/default/theme.lua
@@ -28,7 +28,7 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
-theme.border_focus  = "#3c3836"--"#535d6c"
+theme.border_focus  = "#665c54"--"#535d6c"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
