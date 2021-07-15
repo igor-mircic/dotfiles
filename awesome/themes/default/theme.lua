3805affc110d462dbf39b07e28071c8e4e4ebe37
igor-mircic <igor.mircic@live.com>
2021-07-15 05:53:35 +0200

    Revert "wip"
    This reverts commit 0830a32c8915f5344e418a4d60c6a1ebd2f8b722.

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -2,11 +2,11 @@
 -- Default awesome theme --
 ---------------------------
 
-local theme_assets = R("beautiful.theme_assets")
-local xresources = R("beautiful.xresources")
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
-local gfs = R("gears.filesystem")
+local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 
 local theme = {}
